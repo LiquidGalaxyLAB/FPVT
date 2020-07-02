987cc72eab2f1d9dc3b5fbf5ebe3c07e1e539a5f
CC0210 <charanyac10@gmail.com>
2020-07-02 20:12:01 +0000

    added file set_regions for manually setting parking spaces and added backend docker and rest api

---
+++ Backend/docker_api/signup.py
@@ -0,0 +1,10 @@
+# Create a handler for our read (GET) people
+def read():
+    """
+    This function responds to a request for /api/signup
+
+    :return:        hello
+    """
+    txt = "Sign Up Successful"
+    # Create the list of people from our data
+    return txt
